46b43fb06fb590a916297f9a1b4beeaab510f5da
Fernando Arvelos <fernandoarvelos1@gmail.com>
2023-05-05 10:47:59 -0300

    feat(6) Renderize uma lista com os planetas do Sistema Solar

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,15 +3,13 @@ import PropTypes from 'prop-types';
 
 class Title extends Component {
   render() {
-    const { headlline } = this.props;
-    return (
-      <h2>{ headlline }</h2>
-    );
+    const { headline } = this.props;
+    return <h2>{headline}</h2>;
   }
 }
 
 Title.propTypes = {
-  headlline: PropTypes.string.isRequired,
+  headline: PropTypes.string.isRequired,
 };
 
 export default Title;
