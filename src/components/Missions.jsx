b2f23c2bcd99a13c5e158d4d3a86f228b137a940
Fernando Arvelos <fernandoarvelos1@gmail.com>
2023-05-05 11:14:01 -0300

    feat(10) Renderize uma lista com as missões espaciais

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,12 +1,25 @@
 import { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions">
-        <Title headline="Missões" />
-      </div>
+      <>
+        <div data-testid="missions">
+          <Title headline="Missões" />
+        </div>
+        <div>
+          {missions.map((mission) => (<MissionCard
+            key={ mission.name }
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+          />))}
+        </div>
+      </>
     );
   }
 }
